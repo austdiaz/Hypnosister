9237d4c9f4891c9c54abf0550a390e5e077e9f50
Austin Diaz <austinddiaz@gmail.com>
2015-06-29 23:35:28 -0700

    Original
    Completed lesson

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by Austin Diaz on 6/14/15.
+//  Copyright (c) 2015 Austin Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BNRHypnosisView.h
@@ -0,0 +1,13 @@
+//
+//  BNRHypnosisView.h
+//  Hypnosister
+//
+//  Created by Austin Diaz on 6/14/15.
+//  Copyright (c) 2015 Austin Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRHypnosisView : UIView
+
+@end
